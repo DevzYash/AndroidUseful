c8b3fc3b352de6e5288f8d9b21f991184d8ddaaf
Yash Agarwal <69758267+DevzYash@users.noreply.github.com>
2022-10-04 18:14:58 +0530

    FeedbackServiceConnection.java

--- GoogleFeedbackUtils.java
+++ GoogleFeedbackUtils.java
@@ -1,24 +1,60 @@
-class GoogleFeedbackUtils {
-    private static final String TAG = GoogleFeedbackUtils.class.getSimpleName();
-    public static void bindFeedback(Context context) {
+  class FeedbackServiceConnection {
+    private static int MAX_WIDTH = 800;
+    private static int MAX_HEIGHT = 1700;
+    protected final Window mWindow;
+
+    public FeedbackServiceConnection(Window window) {
+        this.mWindow = window;
+    }
+    public void bindFeedback(Context context){
         Intent intent = new Intent(Intent.ACTION_BUG_REPORT);
         intent.setComponent(new ComponentName("com.google.android.gms", "com.google.android.gms.feedback.LegacyBugReportService"));
-        ServiceConnection serviceConnection = new ServiceConnection() {
+        ServiceConnection service = new ServiceConnection() {
             @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
                 try {
-                    service.transact(Binder.FIRST_CALL_TRANSACTION, Parcel.obtain(), null, 0);
+                    Parcel parcel = Parcel.obtain();
+                    Bitmap bitmap = getScreenshot();
+                    if (bitmap != null) {
+                        bitmap.writeToParcel(parcel, 0);
+                    }
+                    iBinder.transact(IBinder.FIRST_CALL_TRANSACTION, parcel, null, 0);
+                    parcel.recycle();
                 } catch (RemoteException e) {
-                    Log.e(TAG, "RemoteException", e);
+                    Log.e("ServiceConn", e.getMessage(), e);
                 }
             }
             @Override
-            public void onServiceDisconnected(ComponentName name) {}
+            public void onServiceDisconnected(ComponentName componentName) {}
         };
-        context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        context.bindService(intent, service, Context.BIND_AUTO_CREATE);
     }
+     
+
+     public Bitmap getScreenshot() {
+          try {
+              View rootView = mWindow.getDecorView().getRootView();
+              rootView.setDrawingCacheEnabled(true);
+              Bitmap bitmap = rootView.getDrawingCache();
+              if (bitmap != null)
+              {
+                  double height = bitmap.getHeight();
+                  double width = bitmap.getWidth();
+                  double ratio = Math.min(MAX_WIDTH / width, MAX_HEIGHT / height);
+                  return Bitmap.createScaledBitmap(bitmap, (int)Math.round(width * ratio), (int)Math.round(height * ratio), true);
+              }
+          } catch (Exception e) {
+              Log.e("Screenshoter", "Error getting current screenshot: ", e);
+          }
+          return null;
+      }
+
+
 }
 
 
 
-// GoogleFeedbackUtils.bindFeedback(this); this method call from feedback button to launch and use this function.......
+
+
+// Us this in feedback button - new FeedbackServiceConnection(MainActivity.this.getWindow()).bindFeedback(getApplicationContext());
